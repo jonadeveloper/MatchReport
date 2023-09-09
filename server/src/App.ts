b94f55bbf7bd944f7628eb42e38c7509e9323e9a
= <=>
2023-09-09 00:27:18 -0300

    creating routes with endpoints in backend

--- server/src/App.ts
+++ server/src/App.ts
@@ -4,7 +4,13 @@ const app = express();
 const port = 3001;
 
 app.get("/match", (req, res) => {
-  res.send("¡Hola, mundo!");
+  const jsonData = require("../match.json");
+  res.json(jsonData);
+});
+
+app.get("/stats", (req, res) => {
+  const jsonData = require("../stats.json");
+  res.json(jsonData);
 });
 
 app.listen(port, () => {
